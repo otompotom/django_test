6a6a021f6662a554c158ec3f9acf21bf3439bb9d
Dragan Vetnic <dvetnic@yahoo.com>
2018-12-23 17:26:28 +0100

    add email validation logic (emailhunter.co implementation pending)

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,17 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 class UserCreateForm(UserCreationForm):
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        # logic based on emailhunter.co should be implemented here!!!
+        if data == "assa@buc.com":  # dummy logic for testing purposes
+            raise ValidationError("Non existing mail address!!!")
+        return data
+
     class Meta:
         model = get_user_model()
         fields = ("username", "email", "password1", "password2")
